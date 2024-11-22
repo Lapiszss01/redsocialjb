68a9140786f7b906f62999935ad699d5eb24cdb6
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-11-22 16:48:37 +0100

    Index en el welcome ya que va a ser la página principal, factories y seeders creados para la creacion de posts en masa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,29 @@
 <x-app-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
-    <h1>Inicio</h1>
-    <x-slot:sidebar>
-        <p>Home Sidebar</p>
-    </x-slot:sidebar>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="my-4 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Posts
+        </h1>
+
+        <div
+            class="mx-auto mt-8 grid max-w-6xl gap-4 md:grid-cols-2 lg:grid-cols-3"
+        >
+            @foreach($posts as $post)
+                <article
+                    class="flex flex-col overflow-hidden rounded bg-white shadow dark:bg-slate-900"
+                >
+                    <div class="flex-1 space-y-3 p-5">
+
+                        <h2
+                            class="text-xl font-semibold leading-tight text-slate-800 dark:text-slate-200"
+                        >
+                            <a class="hover:underline" href="">
+                                {{ $post->body }}
+                            </a>
+                        </h2>
+                        <img src="{{ asset('images/' . $post->image) }}" alt="Imagen">
+                    </div>
+                </article>
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\Posts\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');;
-Route::view('contacto', 'contact');
-Route::view('blog', 'blog');
-Route::view('nosotros', 'about');
+//Route::view('/', 'welcome')->name('home');;
+Route::get('/', [PostController::class, 'index'])->name('home');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
