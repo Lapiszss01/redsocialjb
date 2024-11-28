7385ff11c439476ba846af4ad1311044b5270a92
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-11-28 10:36:12 +0100

    Retoques en la creacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,11 @@
 <x-app-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
     <div class="mx-auto mt-4 max-w-6xl">
-
-
         <div
             class="px-4 mt-8 grid max-w-4xl gap-4 md:grid-cols-1 lg:grid-cols-1"
         >
-            <form method="POST" action="{{ route('post.store') }}">
-                <article
-                    class="flex flex-col overflow-hidden rounded bg-white shadow dark:bg-slate-900"
-                >
-                    <x-textarea-post-input id="body" name="body"></x-textarea-post-input>
-                    <button type="submit" class="p-2">
-                        Postear
-                    </button>
-                </article>
-                @csrf
-            </form>
+            @auth
+            @include('components.form-create-post')
+            @endauth
 
             @foreach($posts as $post)
                 @include('components.post-article')
