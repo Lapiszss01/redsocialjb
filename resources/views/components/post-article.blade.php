2d8cf5056efef3cf036ecd87ff4defcc60cdee32
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-11-28 17:54:30 +0100

    Created_at en los twitts

--- resources/views/components/post-article.blade.php
+++ resources/views/components/post-article.blade.php
@@ -4,15 +4,17 @@
     <div class="flex-1 space-y-3 p-5">
 
         <h2
-            class="text-xl leading-tight text-slate-800 dark:text-slate-200"
+            class="text-xl leading-tight text-slate-800 dark:text-slate-200 flex justify-between"
         >
             <a class="hover:underline" href="{{route("profile",$post->user->username)}}">
                 {{ $post->user->name }}
             </a>
+            {{$post->created_at}}
+        </h2>
             <p>
                 {{ $post->body }}
             </p>
-        </h2>
+
         @if($post->image != 0)
             <img src="{{ asset('images/' . $post->image) }}" alt="Imagen">
         @endif
