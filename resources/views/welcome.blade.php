2950a943cf6a9eabbdb23f652bccb0d6e9098c70
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2024-12-12 10:47:01 +0100

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
     <div class="mx-auto mt-4 max-w-6xl">
         <div
-            class="px-4 mt-8 grid max-w-4xl gap-4 md:grid-cols-1 lg:grid-cols-1"
+            class=" mt-8 grid gap-4 md:grid-cols-1 lg:grid-cols-1"
         >
             @auth
                 @include('posts-components.form-create-post')
