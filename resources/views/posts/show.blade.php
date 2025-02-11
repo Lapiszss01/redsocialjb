dda82408abbfc84a2ece12a28bf948303367eb38
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2025-02-11 11:56:20 +0100

    Cambio en el show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout :meta-title="$post->title" :meta-description="$post->body">
-    <div class="mx-auto mt-4 max-w-6xl bg-gray-400">
+    <div class="mx-auto mt-4 max-w-6xl">
         @include('posts-components.post-article')
         @include('posts-components.form-create-response-post', ['post' => $post])
     </div>
