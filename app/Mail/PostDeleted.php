b5b556019647eed121b864883ba6535c7b0a598a
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2025-02-12 09:51:28 +0100

    Creando job para enviar email a cliente cuando se borre un post

---
+++ app/Mail/PostDeleted.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PostDeleted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $postBody;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($user, $postBody)
+    {
+        $this->user = $user;
+        $this->postBody = $postBody;
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->subject('Tu post ha sido eliminado')
+            ->view('emails.post_deleted')
+            ->with([
+                'name' => $this->user->name,
+                'postBody' => $this->postBody,
+            ]);
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.deleted-post',
+        );
+    }
+
+}
---
+++ resources/views/emails/post_deleted.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Post Eliminado</title>
+</head>
+<body>
+<p>Hola {{ $name }},</p>
+<p>Tu post titulado "<strong>{{ $postBody }}</strong>" ha sido eliminado.</p>
+<p>Si tienes dudas, contáctanos.</p>
+<p>Saludos,</p>
+<p>El equipo de Blog</p>
+</body>
+</html>
