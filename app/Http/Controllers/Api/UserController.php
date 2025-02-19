0c0633d3fada4b0396608229a569bab462dd0de0
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2025-02-19 16:35:43 +0100

    Otro endpoint para usuarios api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\UserResource;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 
 Route::middleware('api')->group(function () {
+    Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index']);
     Route::get('/posts', [PostController::class, 'index']);
     Route::get('/posts/user/{userId}', [PostController::class, 'getByUser']);
 });
