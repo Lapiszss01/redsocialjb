77b3f428426382b1dffd0be57ada187aa6cbf3f4
Jonathan Betancourt <jonathan.rbm01@gmail.com>
2025-05-06 19:42:17 +0200

    Refactorizacion archivo de rutas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,12 +10,6 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        Dashboard
-                    </x-nav-link>
-                </div>-->
             </div>
 
             <div class="flex items-center space-x-4">
@@ -52,11 +46,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
@@ -66,9 +55,6 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
--- routes/web.php
+++ routes/web.php
@@ -5,42 +5,37 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PDFController;
 use App\Http\Controllers\Posts\PostController;
 use App\Http\Controllers\Posts\UserProfileController;
-use App\Http\Controllers\ProfileController;
+use App\Livewire\Posts\PostForm;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('/{username}',[UserProfileController::class, 'profile'])->name('profile');
-Route::get('/{post}/show',[PostController::class, 'show'])->name('post.show');
-Route::get('/user/{id}/posts/pdf', [PDFController::class, 'generateUserPostsPDF'])->name('user.posts.pdf');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/{post}/show',[PostController::class, 'show'])->name('post.show');
+
+Route::get('/{username}',[UserProfileController::class, 'profile'])->name('profile');
+Route::get('/user/{id}/posts/pdf', [PDFController::class, 'generateUserPostsPDF'])->name('user.posts.pdf');
+Route::get('/pdf/analisis-general', [PdfController::class, 'generatePageAnalysisPDF'])->name('pdf.analisis-general');
+
+Route::middleware(['auth', 'admin'])->get('/admin/users', [AdminUserController::class, 'index'])->name('admin.users');
 
 Route::middleware('auth')->group(function () {
-
     Route::get('/{user}/edit', [UserProfileController::class, 'edit'])->name('userprofile.edit');
     Route::patch('/{user}/update', [UserProfileController::class, 'update'])->name('userprofile.update');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::post('/post.store',[PostController::class, 'store'])->name('post.store');
     Route::post('/{post}/show.store',[PostController::class, 'storeResponse'])->name('post.show.store');
     Route::post('/{post}/like', [PostController::class, 'like'])->name('post.like');
     Route::delete('/{post}/destroy', [PostController::class, 'destroy'])->name('post.destroy');
 
-    Route::post('/upload', [\App\Livewire\Posts\PostForm::class, 'upload'])->name('posts.upload');
+    Route::post('/upload', [PostForm::class, 'upload'])->name('posts.upload');
 
     Route::get('/{user}/notifications]', [NotificationController::class, 'index'])->name('notifications.index');
-
 });
 
-Route::middleware(['auth', 'admin'])->get('/admin/users', [AdminUserController::class, 'index'])->name('admin.users');
-Route::get('/pdf/analisis-general', [PdfController::class, 'generatePageAnalysisPDF'])->name('pdf.analisis-general');
+
+
 
 
 
